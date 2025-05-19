93169aa265659d3e3f1c9386fc7ecbf8f1837f4c
Matt <63430359+MattCmd202@users.noreply.github.com>
2025-05-19 23:50:18 +0800

    ScoreCalculator.java

--- ScoreCalculator.java
+++ ScoreCalculator.java
@@ -3,21 +3,30 @@ import java.util.Scanner;
 public class ScoreCalculator {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-
-        System.out.println("Enter Total of Items: ");
-        double items = scnr.nextDouble();
-
-        while (true) { 
-            
-            System.out.println("Enter Score:");
+        
+        double items=0;
+        
+        while(items==0){
+        
+        System.out.print("Enter Total of Items: ");
+        items = scnr.nextDouble();
+        
+        if (items==0){
+            System.out.println("Items cannot be 0.\n");
+            } 
+        }
+        
+        while(true) {
+        
+            System.out.print("\nEnter Score: ");
             int score = scnr.nextInt();
 
             double FinalScore = (score/items)*40+60;
 
-            if(score > items) {
+            if(score>items) {
                 System.out.println("Score is invalid.");
             } else {
-            System.out.println("Your Score is: "+ FinalScore);
+                System.out.println("Your Score is: "+ FinalScore);
             }
         }
     }
